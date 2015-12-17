371a965efee4b2fcdcea5286fa6cb7c280a5109e
Zony <mo1995106@163.com>
2015-12-17 13:17:22 +0800

    并没有修复并发崩溃的问题。

--- Program/Log.cs
+++ Program/Log.cs
@@ -9,44 +9,41 @@ namespace Zony_Lrc_Download_2._0
     static class Log
     {
         static string CurrentDir;
-        static FileStream logFileStream;
-        static StreamWriter write;
         public static void init_Log()
         {
-            CurrentDir= Environment.CurrentDirectory;
-            logFileStream = new FileStream(CurrentDir + "/log.txt", FileMode.Append);
-            write = new StreamWriter(logFileStream, Encoding.GetEncoding("utf-8")); 
+            CurrentDir = Environment.CurrentDirectory;
         }
         public static void WriteLog(string information)
         {
+            FileStream logFileStream = new FileStream(CurrentDir + "/log.txt", FileMode.Append);
+            StreamWriter write = new StreamWriter(logFileStream, Encoding.GetEncoding("utf-8"));
             write.WriteLine(DateTime.Now.ToString() + "-" + information);
+            write.Close();
+            logFileStream.Close();
         }
-        public static void WriteLog(string name,string info)
+        public static void WriteLog(string name, string info)
         {
-            write.WriteLine(DateTime.Now.ToString() + "-" + "歌曲："+ name + " " + info);
+            FileStream logFileStream = new FileStream(CurrentDir + "/log.txt", FileMode.Append);
+            StreamWriter write = new StreamWriter(logFileStream, Encoding.GetEncoding("utf-8"));
+            write.WriteLine(DateTime.Now.ToString() + "-" + "歌曲：" + name + " " + info);
+            write.Close();
+            logFileStream.Close();
         }
 
         public static string LoadLog()
         {
-            Close();
             FileStream logFileStream = new FileStream(CurrentDir + "/log.txt", FileMode.Open);
             StreamReader read = new StreamReader(logFileStream, Encoding.UTF8);
             StringBuilder strBuilder = new StringBuilder();
             string str;
             while ((str = read.ReadLine()) != null)
             {
-                strBuilder.Append(str+"\r\n");
+                strBuilder.Append(str + "\r\n");
             }
             read.Close();
             logFileStream.Close();
-            init_Log();
             return strBuilder.ToString();
         }
 
-        public static void Close()
-        {
-            write.Close();
-            logFileStream.Close();
-        }
     }
 }

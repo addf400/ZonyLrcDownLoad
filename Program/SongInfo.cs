59f41e0df0163c67212f1f34b19cc1a5736a07ab
Zony <mo1995106@163.com>
2016-01-01 16:23:55 +0800

    使用SongInfo来存储歌曲信息。

---
+++ Program/SongInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using ID3;
+
+namespace Zony_Lrc_Download_2._0
+{
+    public class SongInfo
+    {
+        public string m_SongName { get; set; }
+        public string m_SongSinger { get; set; }
+
+        public void GetSongInfo(string filepath)
+        {
+            try
+            {
+                ID3Info id3 = new ID3Info(filepath, true);
+                m_SongName = id3.ID3v1Info.Title != "" ? id3.ID3v1Info.Title : id3.ID3v2Info.GetTextFrame("TIT2");
+                m_SongSinger = id3.ID3v1Info.Artist != "" ? id3.ID3v1Info.Artist : id3.ID3v2Info.GetTextFrame("TPE1");
+            }catch(System.OverflowException)
+            {
+                m_SongName = "";
+                m_SongSinger = "";
+            }catch(Exception exp)
+            {
+                Log.WriteLog(Log.Class.EXCEPTION, exp.ToString());
+            }
+
+            if(m_SongName == "")
+            {
+                m_SongName = Path.GetFileNameWithoutExtension(filepath);
+            }
+
+            if(m_SongSinger == "")
+            {
+                m_SongSinger = Path.GetFileNameWithoutExtension(filepath);
+            }
+        }
+    }
+}

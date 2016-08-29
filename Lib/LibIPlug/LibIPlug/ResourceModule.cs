869c05afd020f74587ff24b4f765268d3f2efffb
Zony <mo1995106@163.com>
2016-08-29 21:43:09 +0800

    Mod 更新引用资源.

--- Lib/LibIPlug/LibIPlug/ResourceModule.cs
+++ Lib/LibIPlug/LibIPlug/ResourceModule.cs
@@ -1,10 +1,39 @@
-﻿using System.Windows.Forms;
+﻿/*
+ * 描述：资源模型，用与存放插件与主程序的交互资源。
+ * 作者：Zony
+ * 创建日期：2016/08/29
+ * 最后修改日期：2016/08/29
+ * 版本：v1.0
+ */
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace LibIPlug
 {
+    /// <summary>
+    /// 资源模型
+    /// </summary>
     public class ResourceModule
     {
+        /// <summary>
+        /// ListView绑定的右键菜单
+        /// </summary>
         public ContextMenu ListContextMenu { get; set; }
+        /// <summary>
+        /// 存放歌曲文件信息的Listview
+        /// </summary>
         public ListViewNF MainListBox { get; set; }
+        /// <summary>
+        /// 用于存放歌曲文件路径的键值对
+        /// </summary>
+        public Dictionary<int,string> MusicPathList { get; set; }
+        /// <summary>
+        /// 底部进度条
+        /// </summary>
+        public ProgressBar MainProgressBar { get; set; }
+        /// <summary>
+        /// 底部状态栏
+        /// </summary>
+        public StatusStrip MainStatusStrip { get; set; }
     }
 }
--- Lib/SingleLyricSearch/SingleLyricSearch/LibSingleLyricSearch.cs
+++ Lib/SingleLyricSearch/SingleLyricSearch/LibSingleLyricSearch.cs
@@ -1,9 +1,11 @@
 ﻿using LibIPlug;
+using System.Windows.Forms;
+using System.IO;
 
-namespace LibSingleLyricSearch
+namespace LibLibNeteaseTmp
 {
     [PluginInfo("单歌词下载插件", "v1.0", "Zony", "可以下载指定歌曲的歌词。", 1)]
-    public class SingleLyricSearch : IPlugin_Hight
+    public class LibNeteaseTmp : IPlugin_Hight
     {
         public PluginInfoAttribute PluginInfo{ get;set; }
         private ResourceModule module;
@@ -16,8 +18,27 @@ namespace LibSingleLyricSearch
 
         private void bind()
         {
-            module.ListContextMenu.MenuItems.Add("下载歌词");
+            var _currentMenu = module.ListContextMenu.MenuItems.Add("扫描网易云临时文件");
+            _currentMenu.Click += _currentMenu_Click;
             module.MainListBox.ContextMenu = module.ListContextMenu;
+        }
+
+        private void _currentMenu_Click(object sender, System.EventArgs e)
+        {
+            module.MainListBox.Items.Clear();
+            module.MusicPathList.Clear();
+
+            FolderBrowserDialog _fd = new FolderBrowserDialog();
+            _fd.Description = "请选择网易云音乐临时文件的路径:";
+            _fd.ShowDialog();
+            if(_fd.SelectedPath != "")
+            {
+                string[] files = Directory.GetFiles(_fd.SelectedPath, "*.tmp", SearchOption.AllDirectories);
+                foreach(var item in files)
+                {
+                    module.MainListBox.Items.Add(new ListViewItem(new string[] { item,""}));
+                }
+            }
         }
     }
 }
